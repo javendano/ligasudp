4737171b00b6a0bb5c1beeff8ae20bce1a3e54a9
javendano <inhoneh@gmail.com>
2016-05-17 20:55:12 -0300

    cambios varios

--- app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from app.models import Ligue, Team, Player
-# Register your models here.
-
-admin.site.register(Ligue)
-admin.site.register(Team)
-admin.site.register(Player)
